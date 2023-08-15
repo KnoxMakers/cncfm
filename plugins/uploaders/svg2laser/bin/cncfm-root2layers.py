876de95bcdfe2bcdf0f48dd06dba06d58e9d9504
Jondale Stratton <github@serpco.com>
2023-08-15 12:27:13 -0400

    fix for root2layers

--- plugins/uploaders/svg2laser/bin/cncfm-root2layers.py
+++ plugins/uploaders/svg2laser/bin/cncfm-root2layers.py
@@ -6,11 +6,14 @@ class CopyObjects(inkex.Effect):
         inkex.Effect.__init__(self)
 
     def effect(self):
-        cncfm_layer = self.svg.add(Group.new('cncfm', is_layer=True))
+        cncfm_layer = Group.new('cncfm')
+        cncfm_layer.set("inkscape:groupmode", "layer")
+
         for elem in self.document.getroot():
             if elem != cncfm_layer and str(elem) != "layer":
                 cncfm_layer.append(elem)
 
+        self.svg.add(cncfm_layer)
 
 if __name__ == "__main__":
     CopyObjects().run()
